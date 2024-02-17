06476ac809fcc043aada772e79e40b2c652dd903
Jgouken <jjaidenj09@gmail.com>
2024-02-17 09:31:48 -0800

    Telekinetics (incomplete)

--- Assets/Scripts/Telepoint.cs
+++ Assets/Scripts/Telepoint.cs
@@ -15,6 +15,7 @@ public class MousePosition : MonoBehaviour
     [SerializeField] private Transform telepoint;
     [SerializeField] private Magivision magivision;
     [SerializeField] private Cooldown cooldown;
+    [SerializeField] private GameObject teleObject;
     // Gets other objects' components used and such
 
     void Start()
@@ -28,86 +29,106 @@ public class MousePosition : MonoBehaviour
 
     void Update() // Is called once per frame
     {
-        if (Input.GetKey(KeyCode.LeftShift) && !cooldown.IsCoolingDown) // If Left Shift is pressed and the cooldown is over
+        var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var playerHitbox = player.GetComponent<BoxCollider2D>();
+        // Grabs the position of the mouse (relative to screen) and sets the telepoint object relative to the world
+        mouseWorldPos.z = 0f;
+        // The z axis doesn't quite matter in a 2D space
+        telepoint.position = mouseWorldPos;
+        // Sets the position of the telepoint to the world position of the mouse
+
+        if (Input.GetKey(KeyCode.LeftShift)) // If Left Shift is pressed and the cooldown is over
         {
-            var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            var playerHitbox = player.GetComponent<BoxCollider2D>();
-            // Grabs the position of the mouse (relative to screen) and sets the telepoint object relative to the world
-            mouseWorldPos.z = 0f;
-            // The z axis doesn't quite matter in a 2D space
-            telepoint.position = mouseWorldPos;
-            // Sets the position of the telepoint to the world position of the mouse
-
-            var offsetPosition = player.transform.position.y - groundCheck.position.y;
-            // This is the distance between the player's position and what's considered their feet (used as the check to see if they're touching the ground).
-            // This is used as the point where the player is right on top of the ground.
-            RaycastHit2D hitGround = Physics2D.Raycast(telepoint.position, -Vector2.up);
-            RaycastHit2D hitCieling = Physics2D.Raycast(telepoint.position, -Vector2.down);
-            RaycastHit2D hitLeft = Physics2D.Raycast(new Vector2(telepoint.position.x, telepoint.position.y - hitGround.distance + offsetPosition), Vector2.left);
-            RaycastHit2D hitRight = Physics2D.Raycast(new Vector2(telepoint.position.x, telepoint.position.y - hitGround.distance + offsetPosition), Vector2.right);
-            // Shoots raycasts in all directions to tell the distance between the telepoint and ground/platform
-            // Note: Raycasts do not exist when starting inside of an object.
-            Debug.DrawRay(telepoint.position, -Vector2.up * hitGround.distance, Color.red);
-            Debug.DrawRay(telepoint.position, -Vector2.down * hitCieling.distance, Color.red);
-            Debug.DrawRay(new Vector3(telepoint.position.x, (telepoint.position.y - hitGround.distance) + offsetPosition, telepoint.position.z), Vector2.left * hitLeft.distance, Color.red);
-            Debug.DrawRay(new Vector3(telepoint.position.x, (telepoint.position.y - hitGround.distance) + offsetPosition, telepoint.position.z), Vector2.right * hitRight.distance, Color.red);
-            // Shows the rays.
-
-            if (Math.Abs(player.transform.localScale.x) == 1) playerCopy.transform.localScale = player.transform.localScale;
-
-            if (hitGround.collider != null && telepoint.position.y < (telepoint.position.y - hitGround.distance) + (playerHitbox.size.y))
-            // If there is ground under the telepoint and your mouse is close enough to the ground
+            if (!cooldown.IsCoolingDown)
             {
-                if (Vector3.Distance(new Vector3(telepoint.position.x, telepoint.position.y - hitGround.distance), new Vector3(player.transform.position.x, player.transform.position.y + (float)0.743)) <= (magivision.visionSize / 2) - playerHitbox.size.x)
-                // If the telepoint is within the magivision circle
+                var offsetPosition = player.transform.position.y - groundCheck.position.y;
+                // This is the distance between the player's position and what's considered their feet (used as the check to see if they're touching the ground).
+                // This is used as the point where the player is right on top of the ground.
+                RaycastHit2D hitGround = Physics2D.Raycast(telepoint.position, -Vector2.up);
+                RaycastHit2D hitCieling = Physics2D.Raycast(telepoint.position, -Vector2.down);
+                RaycastHit2D hitLeft = Physics2D.Raycast(new Vector2(telepoint.position.x, telepoint.position.y - hitGround.distance + offsetPosition), Vector2.left);
+                RaycastHit2D hitRight = Physics2D.Raycast(new Vector2(telepoint.position.x, telepoint.position.y - hitGround.distance + offsetPosition), Vector2.right);
+                // Shoots raycasts in all directions to tell the distance between the telepoint and ground/platform
+                // Note: Raycasts do not exist when starting inside of an object.
+                Debug.DrawRay(telepoint.position, -Vector2.up * hitGround.distance, Color.red);
+                Debug.DrawRay(telepoint.position, -Vector2.down * hitCieling.distance, Color.red);
+                Debug.DrawRay(new Vector3(telepoint.position.x, (telepoint.position.y - hitGround.distance) + offsetPosition, telepoint.position.z), Vector2.left * hitLeft.distance, Color.red);
+                Debug.DrawRay(new Vector3(telepoint.position.x, (telepoint.position.y - hitGround.distance) + offsetPosition, telepoint.position.z), Vector2.right * hitRight.distance, Color.red);
+                // Shows the rays.
+
+                if (Math.Abs(player.transform.localScale.x) == 1) playerCopy.transform.localScale = player.transform.localScale;
+
+                if (hitGround.collider != null && telepoint.position.y < (telepoint.position.y - hitGround.distance) + (playerHitbox.size.y))
+                // If there is ground under the telepoint and your mouse is close enough to the ground
                 {
-                    if (hitCieling.collider != null) // If there is cieling above the telepoint
+                    if (Vector3.Distance(new Vector3(telepoint.position.x, telepoint.position.y - hitGround.distance), new Vector3(player.transform.position.x, player.transform.position.y + (float)0.743)) <= (magivision.visionSize / 2) - playerHitbox.size.x)
+                    // If the telepoint is within the magivision circle
                     {
-                        if (hitGround.distance + hitCieling.distance >= playerHitbox.size.y)
-                        // if the distance between the cieling and ground is big enough to fit the player
+                        if (hitCieling.collider != null) // If there is cieling above the telepoint
                         {
-                            if (hitLeft.distance > playerHitbox.size.x && hitRight.distance > playerHitbox.size.x)
-                            // If the player is not too close to nearby walls
+                            if (hitGround.distance + hitCieling.distance >= playerHitbox.size.y)
+                            // if the distance between the cieling and ground is big enough to fit the player
                             {
-                                if (Input.GetMouseButtonDown(0)) // Left Click
+                                if (hitLeft.distance > playerHitbox.size.x && hitRight.distance > playerHitbox.size.x)
+                                // If the player is not too close to nearby walls
                                 {
-                                    player.transform.position = new Vector3(telepoint.position.x, (telepoint.position.y - hitGround.distance) + offsetPosition); // Move Player
-                                    cooldown.StartCooldown(); // Start the cooldown before teleporting again
+                                    if (Input.GetMouseButtonDown(0)) // Left Click
+                                    {
+                                        player.transform.position = new Vector3(telepoint.position.x, (telepoint.position.y - hitGround.distance) + offsetPosition); // Move Player
+                                        cooldown.StartCooldown(); // Start the cooldown before teleporting again
+                                    }
+                                    if (telepoint.position.y < (telepoint.position.y - hitGround.distance) + (playerHitbox.size.y * 3)) ActivateCopy(new Vector3(telepoint.position.x, (telepoint.position.y - hitGround.distance) + offsetPosition, telepoint.position.z));
+                                    // Shows the ghost player on the ground if there's enough vertical space
                                 }
-                                if (telepoint.position.y < (telepoint.position.y - hitGround.distance) + (playerHitbox.size.y * 3)) ActivateCopy(new Vector3(telepoint.position.x, (telepoint.position.y - hitGround.distance) + offsetPosition, telepoint.position.z));
-                                // Shows the ghost player on the ground if there's enough vertical space
+                                else playerCopy.SetActive(false);
                             }
                             else playerCopy.SetActive(false);
                         }
-                        else playerCopy.SetActive(false);
-                    }
-                    else
-                    {
-                        if (hitLeft.distance > playerHitbox.size.x && hitRight.distance > playerHitbox.size.x)
-                        // If the player is not too close to any nearby wall
+                        else
                         {
-                            if (Input.GetMouseButtonDown(0)) // Left Click
+                            if (hitLeft.distance > playerHitbox.size.x && hitRight.distance > playerHitbox.size.x)
+                            // If the player is not too close to any nearby wall
                             {
-                                player.transform.position = new Vector3(telepoint.position.x, (telepoint.position.y - hitGround.distance) + offsetPosition, telepoint.position.z); // Move Player
-                                cooldown.StartCooldown(); // Start the coooldown before teleporting again
+                                if (Input.GetMouseButtonDown(0)) // Left Click
+                                {
+                                    player.transform.position = new Vector3(telepoint.position.x, (telepoint.position.y - hitGround.distance) + offsetPosition, telepoint.position.z); // Move Player
+                                    cooldown.StartCooldown(); // Start the coooldown before teleporting again
+                                }
+                                ActivateCopy(new Vector3(telepoint.position.x, (telepoint.position.y - hitGround.distance) + offsetPosition, telepoint.position.z));
+                                // Shows the ghost player on the ground
                             }
-                            ActivateCopy(new Vector3(telepoint.position.x, (telepoint.position.y - hitGround.distance) + offsetPosition, telepoint.position.z));
-                            // Shows the ghost player on the ground
+                            else playerCopy.SetActive(false);
+                            // Same as above
                         }
-                        else playerCopy.SetActive(false);
-                        // Same as above
                     }
+                    else playerCopy.SetActive(false);
                 }
                 else playerCopy.SetActive(false);
             }
-            else playerCopy.SetActive(false);
         }
-        else playerCopy.SetActive(false);
+        else
+        {
+            playerCopy.SetActive(false);
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+        }
     }
 
     void ActivateCopy(Vector3 newPosition)
     {
         playerCopy.SetActive(true);
         playerCopy.transform.position = newPosition;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        teleObject = collision.gameObject;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        teleObject = null;
     }
 }
