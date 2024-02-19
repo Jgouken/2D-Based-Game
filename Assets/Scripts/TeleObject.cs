93a06dc41352684cb44734e5900f08db5f818388
Jgouken <jjaidenj09@gmail.com>
2024-02-18 22:41:48 -0800

    Telekinesis (unfinished...still)

--- Assets/Scripts/TeleObject.cs
+++ Assets/Scripts/TeleObject.cs
@@ -7,14 +7,16 @@ using UnityEngine;
 
 public class TeleObject : MonoBehaviour
 {
-    public BoxCollider2D boundary;
+    public CompositeCollider2D boundary;
     private Vector2 positionOffset;
+    public float boundLeftSide;
     public bool selected = false;
     public bool grabbed = false;
 
     // Start is called before the first frame update
     void Start() {
-        if (!boundary) boundary = transform.parent.gameObject.GetComponent<BoxCollider2D>();
+        if (!boundary) boundary = transform.parent.gameObject.GetComponent<CompositeCollider2D>();
+        boundLeftSide = boundary.bounds.size.y;
     }
     void OnMouseOver()
     {
@@ -38,11 +40,12 @@ public class TeleObject : MonoBehaviour
 
     void Update()
     {
+        if (grabbed) gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0);
         if (!Input.GetMouseButton(0) || Input.GetKey(KeyCode.LeftShift)) { grabbed = false; selected = false; new Vector2(0, 0); }
         if (Input.GetMouseButton(0) && grabbed && !Input.GetKey(KeyCode.LeftShift))
         {
             var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(mousePos.x + positionOffset.x, mousePos.y + positionOffset.y);
+            if (mousePos.x < boundary.GetComponent<Transform>().localPosition.x + (boundary.bounds.size.x / 2) - (boundary.GetComponent<BoxCollider2D>().bounds.size.x / 2)) transform.position = new Vector3(mousePos.x + positionOffset.x, mousePos.y + positionOffset.y);
         }
     }
 }
