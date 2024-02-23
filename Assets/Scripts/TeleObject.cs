fbee3b150efcc360d67cbeec155331fec0ec292a
Jgouken <jjaidenj09@gmail.com>
2024-02-22 17:57:19 -0800

    Update TeleObject.cs

--- Assets/Scripts/TeleObject.cs
+++ Assets/Scripts/TeleObject.cs
@@ -9,16 +9,18 @@ public class TeleObject : MonoBehaviour
 {
     public CompositeCollider2D boundary;
     private Vector2 positionOffset;
-    public bool selected = false;
+    private Vector3 lastMousePos;
+    public Vector2 mouseSpeed;
     public bool grabbed = false;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         if (!boundary) boundary = transform.parent.gameObject.GetComponent<CompositeCollider2D>();
+        lastMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
     void OnMouseOver()
     {
-        selected = true;
         if (Input.GetMouseButtonDown(0))
         {
             var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -32,26 +34,30 @@ public class TeleObject : MonoBehaviour
         if (!Input.GetMouseButton(0))
         {
             positionOffset = new Vector2(0, 0);
-            selected = false;
         }
     }
 
     void Update()
     {
-        if (grabbed) gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0);
-        if (!Input.GetMouseButton(0) || Input.GetKey(KeyCode.LeftShift)) { grabbed = false; selected = false; new Vector2(0, 0); }
-        if (Input.GetMouseButton(0) && grabbed && !Input.GetKey(KeyCode.LeftShift))
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var objectRigidbody = gameObject.GetComponent<Rigidbody2D>();
+        if (grabbed)
         {
-            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(mousePos.x + positionOffset.x, mousePos.y + positionOffset.y);
+            mouseSpeed = (mousePos - lastMousePos) / Time.deltaTime;
+            objectRigidbody.velocity = new Vector3(0, 0);
+            lastMousePos = mousePos;
         }
+        if (!Input.GetMouseButton(0) || Input.GetKey(KeyCode.LeftShift)) { grabbed = false; objectRigidbody.velocity = mouseSpeed; }
+        if (Input.GetMouseButton(0) && grabbed && !Input.GetKey(KeyCode.LeftShift)) transform.position = new Vector3(mousePos.x + positionOffset.x, mousePos.y + positionOffset.y);
+        if (!grabbed) objectRigidbody.velocity = new Vector2(objectRigidbody.velocity.x, objectRigidbody.velocity.y * 0.5f);
     }
 
-    void LateUpdate() {
+    void LateUpdate()
+    {
         var rightEdge = 0.5000f - (gameObject.GetComponent<BoxCollider2D>().bounds.size.x / (boundary.bounds.size.x * 2));
-        var leftEdge = (gameObject.GetComponent<BoxCollider2D>().bounds.size.x / (boundary.bounds.size.x * 2)) -0.5000f;
+        var leftEdge = (gameObject.GetComponent<BoxCollider2D>().bounds.size.x / (boundary.bounds.size.x * 2)) - 0.5000f;
         var topEdge = 0.5000f - (gameObject.GetComponent<BoxCollider2D>().bounds.size.y / (boundary.bounds.size.y * 2));
-        var bottomEdge = (gameObject.GetComponent<BoxCollider2D>().bounds.size.y / (boundary.bounds.size.y * 2)) -0.5000f;
+        var bottomEdge = (gameObject.GetComponent<BoxCollider2D>().bounds.size.y / (boundary.bounds.size.y * 2)) - 0.5000f;
 
         transform.localPosition = new Vector3(transform.localPosition.x > rightEdge ? rightEdge : transform.localPosition.x < leftEdge ? leftEdge : transform.localPosition.x, transform.localPosition.y > topEdge ? topEdge : transform.localPosition.y < bottomEdge ? bottomEdge : transform.localPosition.y);
     }
